4338de502ce62c37b6e2d37e26c747e56da80a09
MohammadSalem-7 <hagerzaki319@gmail.com>
2025-05-21 15:27:24 +0300

    Delete payment.js

--- payment.js
+++
@@ -1,22 +0,0 @@
-<!DOCTYPE html>
-<html lang="ar">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>إتمام الدفع</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <nav>
-            <div class="logo">إتمام الدفع 💳</div>
-        </nav>
-    </header>
-
-    <section class="payment-container">
-        <h2>💳 اختر طريقة الدفع</h2>
-        <button class="btn">💳 الدفع عبر باي بال</button>
-        <button class="btn">💳 الدفع ببطاقة الائتمان</button>
-    </section>
-</body>
-</html>
